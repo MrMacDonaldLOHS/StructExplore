8e9680ee934c1da35104cbcffc749a5b0300cba1
Keith <macdonak@loswego.k12.or.us>
2022-01-30 09:36:16 -0800

    Initial version

---
+++ Explore5.cpp
@@ -0,0 +1,37 @@
+
+#include <iostream>
+using namespace std;
+struct Point {
+	double x;
+	double y;
+	Point();
+	Point(double xVal, double yVal);
+
+};
+Point::Point() {
+	x = 0;
+	y = 0;
+}
+
+Point::Point(double xVal, double yVal) {
+	x = xVal;
+	y = yVal;
+}
+
+void PrintPoint(Point p) {
+    cout << ": x = " << p.x << ", y = " << p.y << endl;
+}
+
+
+// Nothing else to do
+int main() {
+    Point a(3,9);
+    Point b(4.5, 7.8);
+    cout << "a";
+    PrintPoint(a);
+    cout << "b";
+    PrintPoint(b);
+    return 0;
+}
+
+
