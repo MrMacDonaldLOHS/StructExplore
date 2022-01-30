c03ce88f5c446365502a9fbcfe5fbc365824c826
Keith <macdonak@loswego.k12.or.us>
2022-01-30 09:20:12 -0800

    Initial design

---
+++ Practice1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// Put the person struct here
+
+
+
+
+
+
+int main() {
+	// Create two instances of person here, and assign the names.
+
+
+
+
+
+
+	// Add the prints here
+
+
+
+
+	return 0;
+}
